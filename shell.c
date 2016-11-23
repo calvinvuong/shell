30777365dc2cdaf98cb5b54dddcb13c4a702f25d
Calvin Vuong <cvuong@stuy.edu>
2016-11-23 14:46:11 -0500

    started sorting code into separate functions

--- shell.c
+++ shell.c
@@ -3,6 +3,36 @@
 #include <unistd.h>
 #include <string.h>
 
+
+// takes an array of parsed arguments
+// forks a child process to execute these arguments, waits
+void execute(char *args[]) {
+  int f = fork();
+  if ( f == 0 )
+    execvp(args[0], args);
+  else
+    wait(&f);
+}
+
+/*
+// takes a string of characters
+// splits them based on a given delimiter
+// returns an array of split strings
+char ** split(char input[], char delimiter[]) {
+  char *ret[1000];
+  char *t = input;
+
+  int i = 0;
+  while ( t != NULL ) {
+    ret[i] = strsep(&t, delimiter);
+    i++;
+  }
+  ret[i] = NULL;
+
+  return ret;
+}
+*/
+    
 int main() {
 
   while ( 1 ) {
@@ -33,13 +63,8 @@ int main() {
       execvp(command[0], command);
     }
     */
-    
-      int f = fork();
-      if (f == 0 ) {
-	execvp(command[0], command);
-      } else {
-	wait(&f);
-      }
+      execute(command);
+
     }
 
   }
