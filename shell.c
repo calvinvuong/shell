030e8b73c856101e7d86a99fa26fae3198435fb7
williamx <wxiang@stuy.edu>
2016-11-24 16:44:04 -0500

    fixed whitespace issue

--- shell.c
+++ shell.c
@@ -3,14 +3,14 @@
 #include <unistd.h>
 #include <string.h>
 
-/**************************************
+/*******************************************
 EXECUTE: forks child to exec command, waits
 * Input:
     > args: array of char pointers
         > args[0] is command
         > args[1] and on are args
-* Output: void
-**************************************/
+	* Output: void
+	********************************************/
 void execute(char *args[]) {
   int f = fork();
   if ( f == 0 )
@@ -22,15 +22,15 @@ void execute(char *args[]) {
 
 
 
-/**************************************
+/******************************************
 SPLIT: splits input str on given delimiter
 * Input:
     > input: string to parse
     > delimiter: i wonder what this is
-* Output:
+    * Output:
     > "array" of pointers to split strings 
-**************************************/
-char ** split(char input[], char delimiter[]) {
+*******************************************/
+char ** split(char * input, char * delimiter) {
   char ** ret = (char **) malloc(1000);
   char *t = input;
 
@@ -40,10 +40,29 @@ char ** split(char input[], char delimiter[]) {
     i++;
   }
   ret[i] = 0; //null term for both exec and cmd parsing
-
   return ret;
 }
 
+
+/*************************************************
+WHITESPACE-BEGONE: condenses whitespace in string
+* Input:
+    > input: string to condence
+    * Output:
+    > "array" of pointers to split strings 
+**************************************************/
+char * whitespaceBeGone( char * input ) {
+  char * newStr = (char *) malloc(1000 * sizeof(char));
+
+  int i = 0;
+  for (; input[i] != '\0'; i++) {
+    if ( !(input[i] == ' ' && (strlen(newStr)==0 || input[i+1] == '\0' || input[i+1] == ' ')) ) {
+      sprintf(newStr, "%s%c", newStr, input[i]);
+    }
+  }
+
+  return newStr;
+}
 
 
 int main() {
@@ -54,15 +73,25 @@ int main() {
     fgets(input, sizeof(input), stdin);
     *strchr(input, '\n') = 0; //get rid of newline
 
-    char **commands = (char **) malloc(1000);  //list of commands separated by semicolons
-    char *t = input;
+    //list of commands separated by semicolons
+    char **commands = (char **) malloc(1000);
     commands = split(input, ";");
  
     int i;
     for (i = 0; commands[i] != NULL; i++) { 
-      
-      char ** command = split(commands[i], " ");
-      
+      char * command_nonsplit = (char *) malloc(1000);
+      char ** command = (char **) malloc(1000);
+
+      command_nonsplit = whitespaceBeGone( commands[i] );
+      command = split(command_nonsplit, " ");
+
+      /*
+	printf("\tENTIRE CMD: (%s)\n", command_nonsplit);
+	printf("\tCOMMAND: (%s)\n", command[0]);
+	printf("\tARG 1: (%s)\n", command[1]);
+	printf("\tARG 2: (%s)\n", command[2]);
+      */      
+
       if( !strcmp(command[0], "exit") ) {
 	printf("Exiting shell...\n");
 	exit(0);
@@ -72,14 +101,18 @@ int main() {
 	execvp(command[0], command);
 
       } else {
-	printf("COMMAND: %s\n", command[0]);
-	printf("FIRST ARG: %s\n", command[1]);
 	execute(command);
       }
-    }  
-  }
 
-  //FREE STUFF LATER
+      free(command_nonsplit);
+      free(command);
+      command_nonsplit = NULL;
+      command = NULL;
+    }  
+
+    free(commands);
+    commands = NULL;
+  }
 
   return 0;
 }
